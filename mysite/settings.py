50e013980f29b4435ae47d7d3706c418465b1bd1
Akash Gupta <akashgupta93366@gmail.com>
2019-03-02 23:26:11 +0530

    whitenoise added4

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	'whienoise.middleware.WhiteNoiseMiddleWare',
+	'whitenoise.middleware.WhiteNoiseMiddleWare',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
