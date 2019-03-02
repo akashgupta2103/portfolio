71c7f65784dc32faa87112b0c9c01b031d4755b9
Akash Gupta <akashgupta93366@gmail.com>
2019-03-02 23:54:55 +0530

    added default postgres in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 
 db_from_env = dj_database_url.config()
-DATABASES[‘default’].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,16 +83,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD':'django1234',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
-}
+
 
 
 # Password validation
