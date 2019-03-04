68d61a5b1df8be5220ed4c95784985d6c0ff8473
Akash Gupta <akashgupta93366@gmail.com>
2019-03-04 22:12:36 +0530

    aws1

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 	'skills',
 	'interests',
 	'experiences',
+	'storages',
 ]
 
 MIDDLEWARE = [
@@ -136,5 +137,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "") 
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+AWS_QUERYSTRING_AUTH = False 
+AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", "")
+MEDIA_ROOT = os.environ.get("MEDIA_URL", "")
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR
+
